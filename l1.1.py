369745cfee08e45ce4732045db862a33c61cf0e0
yaninass <yaninatolkacheva20@gmail.com>
2023-09-04 20:59:41 +0300

    code new

--- l1.1.py
+++ l1.1.py
@@ -4,5 +4,5 @@ for digit in str(number):
     if int(digit) > max_digit:
         max_digit = int(digit)
 print(max_digit)
-reverse_number=str(number)[::-1]
+reverse_number = str(number)[::-1]
 print(reverse_number)
