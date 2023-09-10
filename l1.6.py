523c74cd9def72375e3c8719b006cd77f14d6263
yaninass <yaninatolkacheva20@gmail.com>
2023-09-10 21:57:49 +0300

    code new

---
+++ l1.6.py
@@ -0,0 +1,7 @@
+input_str = input("Введите кортеж целых чисел : ")
+list_of_numbers = [int(item.strip()) for item in input_str.split()]
+tuple_of_numbers = tuple(list_of_numbers)
+maximum_value = max(tuple_of_numbers)
+minimum_value = min(tuple_of_numbers)
+print(f"Максимальное значение: {maximum_value}")
+print(f"Минимальное значение: {minimum_value}")
