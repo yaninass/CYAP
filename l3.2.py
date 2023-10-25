86fecf050054dc0ac151a6c3fa29191b1bf218af
yaninass <yaninatolkacheva20@gmail.com>
2023-10-26 02:51:09 +0300

    new lab

--- l3.2.py
+++ l3.2.py
@@ -5,7 +5,7 @@ students8=[]
 for line in lines:
     data=line.split()
     surname=data[0]
-    grades=list(map(int,data[1:]))
+    grades=list(map(int,data[1:]))# тут кароче пробегаем по списку берём фамилию и потом оценки
     avg_grade=sum(grades)/len(grades)
     if avg_grade>8:
         students8.append(surname)
---
+++ l3.4.py
@@ -0,0 +1,15 @@
+import json
+firms_profit={}#этот словарь хранит прибыль каждой компании
+profits=[]#список чтобы хранить прибыль всех +
+with open("firms.txt","r",encoding="utf-8") as file:
+    lines=file.readlines()
+    for line in lines:
+        name,d,revenue,cost=line.split()#d нам не нужно
+        profit=int(revenue)-int(cost)
+        firms_profit[name]=profit
+        if profit>0:
+            profits.append(profit)
+avarage_profit=sum(profits)/len(profits) if profits else 0
+result=[firms_profit,{"avarage_profit": avarage_profit}]# список хранит 2 словаря компании+прибыль, средняя прибыль
+with open("firms_profit.txt","w",encoding="utf-8") as outfile:
+    json.dump(result,outfile,ensure_ascii=False,indent=4)
